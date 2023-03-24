6efda7b8d4d1fbce7970c789c756833a349af253
ivm25 <ishanvm25@gmail.com>
2023-03-24 13:23:43 +1100

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+
+import pandas as pd
+import numpy as np
+
+import plotly.express as px
+import seaborn as sns
+
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_absolute_error
