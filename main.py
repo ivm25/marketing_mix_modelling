f0c6690684739bc1a60fdf7e0624b154dad22b87
ivm25 <ishanvm25@gmail.com>
2023-04-19 08:10:35 +1000

    Update main.py
    added script features to undertake basic Multiple linear regression modelling

--- main.py
+++ main.py
@@ -81,7 +81,15 @@ correlation_df\
 # Basic Adstock Model ----
 
 def adstock(series, rate):
-    
+    """_summary_
+
+    Args:
+        series (_type_): _description_
+        rate (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     tt = np.empty(len(series))
     for i in range(1, len(series)):
         tt[i] = series[i] + series[i-1]*rate
@@ -105,9 +113,9 @@ adstock_facebook_s = adstock(correlation_df.facebook_s, 0.5)
 # prep fpr modelling
 
 X = pd.concat([adstock_tv_s, 
-               adstock_search_s, 
-               adstock_ooh_s,
+               adstock_ooh_s, 
                adstock_print_s,
+               adstock_search_s,
                adstock_facebook_s,
                correlation_df.competitor_sales_b,
                pd.get_dummies(correlation_df.date_month)
@@ -188,10 +196,10 @@ def adstock_search(df,
         adstock_search_s_new = adstock(df.search_s, search)
         
         x_new = pd.concat([adstock_tv_s_new, 
-                       adstock_facebook_s_new, 
-                       adstock_ooh_s_new,
-                       adstock_search_s_new,
+                       adstock_ooh_s_new, 
                        adstock_print_s_new,
+                       adstock_search_s_new,
+                       adstock_facebook_s_new,
                        df.competitor_sales_b,
                        pd.get_dummies(df.date_month)],
                        axis = 1)
@@ -238,3 +246,184 @@ best_model = adstock_search(correlation_df[0:100],
 
 best_model_coef = best_model['model'][0]['lm2'].coef_
 best_model_coef_names = X.columns
+
+rebalancing_coef_df = pd.DataFrame(dict(name = best_model_coef_names,
+                                        value = best_model_coef))\
+                                            .set_index('name')\
+                                                .filter(regex = "_s$",
+                                                        axis = 0)\
+                                                            .assign(value = lambda x:x['value']/np.sum(x['value']))\
+                                                                .reset_index()
+                                                                
+                                                                
+# Media spend rebabalanced
+
+total_current_spend = media_spend_df["spend_current"].sum()
+
+media_spend_rebalanced_df = media_spend_df.copy()
+
+media_spend_rebalanced_df["spend_new"] = total_current_spend * (rebalancing_coef_df['value'])
+
+
+media_spend_rebalanced_df\
+    .melt(id_vars = 'media')\
+        .pipe(px.bar,
+              x = 'media',
+              y = 'value',
+              color = 'media',
+              barmode = 'group',
+              template = 'plotly_dark'
+              )
+
+    
+# Predcted Revenue after rebalancing
+
+X_train_adstock = x_train[x_train.columns[x_train.columns.str.startswith('adstock_')]]
+
+X_train_not_adstock = x_train[x_train.columns[~x_train.columns.str.startswith('adstock_')]]
+
+X_train_adstock_rebal = X_train_adstock.copy()
+
+X_train_adstock_rebal.sum(axis = 1)
+
+for i, col in enumerate(X_train_adstock_rebal.columns):
+    X_train_adstock_rebal[col] = X_train_adstock_rebal.sum(axis = 1) * rebalancing_coef_df['value'][i]
+
+
+X_train_rebal = pd.concat([X_train_adstock_rebal, X_train_not_adstock], axis = 1)
+
+predicted_revenue_current = best_model['model'][0].predict(x_train).sum()
+
+predicted_revenue_new = best_model['model'][0].predict(X_train_rebal).sum()
+
+predicted_revenue_new / predicted_revenue_current
+
+
+# Step 4 Budget Optimisation
+
+rng = np.random.default_rng(123)
+size = 100
+budget_grid_df = pd.DataFrame(dict(
+    adstock_tv_s = rng.uniform(0, 1, size = size),
+    adstock_ooh_s = rng.uniform(0 , 1, size = size),
+    adstock_print_s = rng.uniform(0 , 1, size = size),
+    adstock_search_s = rng.uniform(0, 1, size  = size),
+    adstock_facebook_s = rng.uniform(0, 1, size = size)
+    
+))
+
+
+budget_grid_df
+
+for i, row in enumerate(budget_grid_df.index):
+    print(budget_grid_df.loc[i,:]/np.sum(budget_grid_df.loc[i,:]))
+    
+
+
+def optimize_budget(df, 
+                    grid, 
+                    media_spend,
+                    verbose = True):
+    
+    X_train = df
+    budget_grid_df = grid
+    media_spend_df = media_spend
+    
+    best_budget = dict(
+        rebalancing_coef = None,
+        media_spend_rebal = None,
+        score = None
+    )
+    
+    for i , row in enumerate(budget_grid_df.index):
+        
+        # scale the randome budget mix
+        budget_scaled = budget_grid_df.loc[i,:]/np.sum(budget_grid_df.loc[i,:])
+        
+        # create rebalancing coefficients
+        
+        rebalancing_coef_df = budget_scaled\
+            .to_frame()\
+                .reset_index()\
+                    .set_axis(['name','value'], axis = 1)
+       
+    # Rebalance adstock
+    
+    X_train_adstock = X_train[X_train.columns[X_train.columns.str.startswith('adstock_')]] 
+    
+    X_train_not_adstock = X_train[X_train.columns[~X_train.columns.str.startswith('adstock_')]]
+    
+    X_train_adstock_rebal = X_train_adstock.copy()
+    
+    X_train_adstock_rebal.sum(axis = 1)
+    
+    
+    for i, col in enumerate(X_train_adstock_rebal.columns):
+        X_train_adstock_rebal[col] = X_train_adstock_rebal.sum(axis = 1) * rebalancing_coef_df['value'][i]
+        
+    
+    X_train_rebal = pd.concat([X_train_adstock_rebal, X_train_not_adstock], axis = 1)
+    
+    # Make Predictions
+    predicted_revenue_current = best_model['model'][0].predict(X_train).sum()
+    
+    predicted_revenue_new = best_model['model'][0].predict(X_train_rebal).sum()
+    
+    score = predicted_revenue_new/predicted_revenue_current
+    
+    # Media spend rebalanced
+    
+    total_current_spend = media_spend_df['spend_current'].sum()
+    
+    media_spend_rebalanced_df = media_spend_df.copy()
+    
+    media_spend_rebalanced_df['spend_new'] = total_current_spend * (rebalancing_coef_df['value'])
+    
+    if best_budget['score'] is None or score > best_budget['score']:
+        best_budget['rebalancing_coef'] = rebalancing_coef_df,
+        best_budget['media_spend_rebal'] = media_spend_rebalanced_df
+        best_budget['score'] = score
+        
+        if verbose:
+            print("New Best Budget:")
+            print(best_budget)
+            print("\n")
+            
+    if verbose:
+        print("Done!")
+    
+    return best_budget
+          
+                    
+budget_optimized = optimize_budget(
+    df          = x_train,
+    grid        = budget_grid_df, 
+    media_spend =  media_spend_df,
+    verbose     = True
+)              
+    
+budget_optimized['media_spend_rebal']\
+    .melt(id_vars = 'media')\
+        .pipe(px.bar,
+              x = 'variable',
+              y = 'value',
+              color = 'media',
+              barmode = 'group',
+              template = 'plotly_dark')
+
+# Save work
+
+from joblib import dump, load
+
+dump(best_model, "artefacts/best_model")
+
+load("artefacts/best_model")
+
+dump(media_spend_df,"artefacts/media_spend_df")
+load("artefacts/media_spend_df")
+
+dump(x_train,"artefacts/X_train")
+
+load("artefacts/X_train")
+
+
