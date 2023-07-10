06d3aa8ad3adcd60ac0be18cd62d76615e8d3150
ivm25 <ishanvm25@gmail.com>
2023-07-10 14:23:09 +1000

    Update app.py

--- app.py
+++ app.py
@@ -201,7 +201,7 @@ app.layout = html.Div(children=[navbar,
     )),dbc.Col(dcc.Dropdown(
         id='dropdown_adstck',
         options=list(key_coefficients_dict.keys()),
-        value=0.5,
+        value=0.1791267574878015,
          style={"width": "600px"},
         
        
