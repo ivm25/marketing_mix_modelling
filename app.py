b929d37e798c0369988002c0fdbe9bf346d6c453
ivm25 <ishanvm25@gmail.com>
2023-08-17 13:43:08 +1000

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,9 @@ app.layout = html.Div(children=[navbar,
         
        
        
-    )),dbc.Col(dcc.Dropdown(
+    )),dbc.Col(dcc.Graph(id="graph", style = {'width': '600px'}),lg=6, md=12, sm = 24)]),
+   
+    dbc.Row([dbc.Col(dcc.Dropdown(
         id='dropdown_adstck',
         options=list(key_coefficients_dict.keys()),
         value=0.1791267574878015,
@@ -107,10 +109,8 @@ app.layout = html.Div(children=[navbar,
         
        
        
-    ))]),
-   
-    dbc.Row([dbc.Col(dcc.Graph(id="graph", style = {'display': 'inline-block'}),lg=6, md=12, sm = 24),
-    dbc.Col(dcc.Graph(id = "graph_2", style ={'display': 'inline-block'}),lg=6, md=12, sm = 24)]),
+    )),
+    dbc.Col(dcc.Graph(id = "graph_2", style ={'width': '600px'}),lg=6, md=12, sm = 24)]),
     dbc.Row([dbc.Col(html.P("Select a variable to understand data distribution:")), dbc.Col(html.P("Select a time period to understand seasonality:"))]),
     dbc.Row([dbc.Col(dcc.Dropdown(
         id='dropdown_heatmap',
